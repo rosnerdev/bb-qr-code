fc0de56653cd1ae2dea91a7816e359e7c7aeb381
rosnerdev <yrosner123@gmail.com>
2022-12-04 21:08:23 +0200

    commit 1

--- src/Component.svelte
+++ src/Component.svelte
@@ -3,11 +3,15 @@
   import QrCode from "svelte-qrcode"
 
   export let text
+  export let size
+  export let color
+  export let backgroundColor
+  export let padding
 
   const { styleable } = getContext("sdk")
   const component = getContext("component")
 </script>
 
 <div use:styleable={$component.styles}>
-  <QrCode value={text} />
+  <QrCode value={text} size={size} color={color} background={backgroundColor} padding={padding} />
 </div>
